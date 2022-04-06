5a2a4a6718c65d99ccb56718e47266ea48a2c83a
AdwitiyaDas <lovebeingme.adwi@gmail.com>
2022-04-06 12:42:38 +0530

    Removed Main Page Component, which was not being used

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { LoginComponent } from './login/login.component';
-import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
 {path: '', component: LoginComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { FeedbackComponent } from './feedback/feedback.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DatePipe } from '@angular/common'
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MainPageComponent } from './main-page/main-page.component';
 import { LoginComponent } from './login/login.component';
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -18,7 +17,6 @@ import {MatTooltipModule} from '@angular/material/tooltip';
   declarations: [
     AppComponent,
     FeedbackComponent,
-    MainPageComponent,
     LoginComponent,
   ],
   imports: [
